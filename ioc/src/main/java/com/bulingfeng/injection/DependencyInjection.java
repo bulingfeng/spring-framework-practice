684fc974be7fdb9c1a730b08a7a95d40dc0dcd78
bulingfeng <413957294@qq.com>
2022-07-02 16:29:14 +0800

    添加参考文档

--- ioc/src/main/java/com/bulingfeng/injection/DependencyInjection.java
+++ ioc/src/main/java/com/bulingfeng/injection/DependencyInjection.java
@@ -5,6 +5,7 @@ import com.bulingfeng.repository.UserRepository;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
 
 import java.util.Collection;
 
@@ -19,5 +20,12 @@ public class DependencyInjection {
         System.out.println("user的集合为:"+userRepository.getCollection());
         ObjectFactory<User> objectFactory=userRepository.getObjectFactory();
         System.out.println("objectFactory中获取到的对象为:"+objectFactory.getObject());
+
+        /**
+         * bean大致能够分为两种,内置的bean和容器内置的bean
+         */
+        // 容器中内置的bean
+        Environment environment=beanFactory.getBean(Environment.class);
+        System.out.println(" 内建 environment 对象 "+environment);
     }
 }
